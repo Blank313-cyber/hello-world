8d099cc284cb1d083604a2c49be2147b2a24dc6e
Blank313-cyber <lewisdinax1@gmail.com>
2024-08-20 09:24:57 +0000

    added index page

---
+++ hello-world/app/index.jsx
@@ -0,0 +1,16 @@
+
+import {View,Text, SafeAreaView} from 'react-native'
+
+
+const HelloWorld = () =>{
+
+    return(
+        <SafeAreaView>
+            <Text>
+                HEllow world
+            </Text>
+        </SafeAreaView>
+ )
+}
+
+export default HelloWorld
